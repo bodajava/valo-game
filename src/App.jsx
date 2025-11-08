3d88c7e6b0ccc4be745281c33da4e525a3eebb35
bodajava <bbido761@gmail.com.com>
2025-11-08 23:08:15 +0200

    Replace ScrollSmoother with Lenis (free smooth scroll library)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import NavBar from './components/NavBar';
@@ -6,18 +6,43 @@ import Features from './components/Features';
 import Story from './components/Story';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-
+import Lenis from 'lenis';
 
 export default function App() {
-  return <>
-      <main className="relative min-h-screen w-screen overflow-x-hidden">
-        <NavBar />
-        <Hero />
-        <About />
-        <Features />
-        <Story />
-        <Contact />
-        <Footer />
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
-  </>
+  )
 }
